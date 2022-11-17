d4a8cb1753868db10d6ce3cfe9f5d6153084e656
RyandTeague <ryanteague@hotmail.co.uk>
2022-11-17 10:59:49 +0000

    add technician model

--- bookingsystem/models.py
+++ bookingsystem/models.py
@@ -15,4 +15,15 @@ class User(models.Model):
     updated_at = models.DateTimeField(auto_now=True, null=True)
     
     def __str__(self):
-    return self.username
+        return self.username
+
+class Technician(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=25)
+    last_name = models.CharField(max_length=25)
+    avatar_url = models.CharField(max_length=260, null=True)
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True)
+    def __str__(self):
+        return f’Technician {self.first_name} {self.last_name}’
