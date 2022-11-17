beeace28c45d1ac227a38ece61ad2dd7fb83bbc3
RyandTeague <ryanteague@hotmail.co.uk>
2022-11-17 10:52:54 +0000

    add user model

--- bookingsystem/models.py
+++ bookingsystem/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    username = models.CharField(max_length=30, unique=True)
+    email = models.EmailField(max_length=254, unique=True)
+    phone_number = models.CharField(max_length=20, unique=True)
+    password = models.CharField(max_length=20)
+    otp_code = models.CharField(max_length=6, unique=True, null=True)
+    email_verified = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
+    is_superadmin = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True)
+    
+    def __str__(self):
+    return self.username
