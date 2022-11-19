e133a28c78ce230201ebca8393f134e721a66705
RyandTeague <ryanteague@hotmail.co.uk>
2022-11-19 02:04:57 +0000

    add authentication models

--- nailsalon2022/urls.py
+++ nailsalon2022/urls.py
@@ -15,30 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.contrib.auth import views as auth_views
-from bookingsystem import views
-from bookingsystem import views as users_views
-
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('/treatments/<uuid:treatment_id>/checkin',views.treatment_checkin, name = 'treatment_checkin'),
-    # path('/treatments/<uuid:treatment_id>/checkout',views.treatment_checkout, name = 'treatment_checkout'),
-    path('treatments/', views.treatments, name='treatments'),
-    path('treatments/<uuid:treatment_id>/', views.treatments_detailed_view, name='treatments_detailed_view'),
-    path('treatments/<uuid:treatment_id>/booking/', views.booking, name='booking'),
-    path('', views.homepage, name='salon-home'),
-    path('about/', views.about, name='salon-about'),
-    path('dashboard', views.dashboard, name='dashboard'),
-    path('admin', views.admin_list, name='admin-list'),
-    path('admin/create', users_views.admin_create, name='register'),
-    path('profile/', users_views.profile, name='profile'),
-    path('/admin/create', views.admin_create, name='admin-create'),
-    path('/admin/<uuid:staff_id>', views.show_admin, name= 'admin-staff'),
-    path('/admin/<uuid:staff_id>/edit', views.edit_admin, name= 'admin-staff-edit'),
-    path('/admin/<uuid:staff_id>/delete', views.delete_admin, name='admin-staff-delete'),
-    path('login', auth_views.LoginView.as_view(template_name = 'salon/login.html'), name= 'admin-login'),
-    path('logout', auth_views.LogoutView.as_view(template_name= 'salon/logout.html'), name= 'admin-logout'),
-    path('/admin/logs',views.logs, name = 'logs')
 ]
