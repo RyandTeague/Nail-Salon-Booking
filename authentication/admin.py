3bbaf79636491f9162658a27eff78c52ac57ff30
RyandTeague <ryanteague@hotmail.co.uk>
2022-11-19 12:14:37 +0000

    change file names

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from authentication.models import Customer, RoomManager, Contact, Rooms, Booking
+from authentication.models import Customer, RoomManager
+from booking.models import Contact, Rooms, Booking
 
 admin.site.register(Customer)
 admin.site.register(RoomManager)
