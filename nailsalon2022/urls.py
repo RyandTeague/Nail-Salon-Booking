bafe7c91e1dfeac9ed8eb001fc490c48a99aef4c
RyandTeague <ryanteague@hotmail.co.uk>
2022-11-19 09:50:46 +0000

    add base urls

--- nailsalon2022/urls.py
+++ nailsalon2022/urls.py
@@ -14,8 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from authentication import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('',include('booking.urls')),
+    path('user/',include('authentication.urls')),
+    path('manager/',include('authentication.urls')),
+    path('logout',views.logout,name='logout'),
+    path('api/',include('api.urls')),
+    path('',include('payment.urls'))
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
