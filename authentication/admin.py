ebbe51b025f9c51629790f41494061dbcdbc69d1
RyandTeague <ryanteague@hotmail.co.uk>
2022-11-19 10:29:55 +0000

    add views to apps

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from authentication.models import Customer, RoomManager, Contact, Rooms, Booking
 
-# Register your models here.
+admin.site.register(Customer)
+admin.site.register(RoomManager)
