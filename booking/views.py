c5ddc806af70a5ccd62a23a95d2e29397bf3a5d8
RyandTeague <ryanteague@hotmail.co.uk>
2022-11-20 10:39:14 +0000

    write other views

--- booking/views.py
+++ booking/views.py
@@ -182,6 +182,63 @@ def userUpdateSubmit(request, id):
         'times':hour,
         'id': id,
     })
+
+def staffPanel(request):
+    today = datetime.today()
+    minDate = today.strftime('%Y-%m-%d')
+    deltatime = today + timedelta(days=21)
+    strdeltatime = deltatime.strftime('%Y-%m-%d')
+    maxDate = strdeltatime
+    #Only show the Appointments 21 days from today
+    items = Appointment.objects.filter(day__range=[minDate, maxDate]).order_by('day', 'time')
+
+    return render(request, 'staffPanel.html', {
+        'items':items,
+    })
+
+def dayToWeekday(x):
+    z = datetime.strptime(x, "%Y-%m-%d")
+    y = z.strftime('%A')
+    return y
+
+def validWeekday(days):
+    #Loop days you want in the next 21 days:
+    today = datetime.now()
+    weekdays = []
+    for i in range (0, days):
+        x = today + timedelta(days=i)
+        y = x.strftime('%A')
+        # Append only weekdays 
+        if y == 'Monday' or y == 'Tuesday' or y == 'Wednesday' or y == 'Thursday' or y == 'Friday':
+            weekdays.append(x.strftime('%Y-%m-%d'))
+    return weekdays
+    
+def isWeekdayValid(x):
+    validateWeekdays = []
+    for j in x:
+        if Appointment.objects.filter(day=j).count() < 10:
+            validateWeekdays.append(j)
+    return validateWeekdays
+
+def checkTime(times, day):
+    #Only show the time of the day that has not been selected before:
+    x = []
+    for k in times:
+        if Appointment.objects.filter(day=day, time=k).count() < 1:
+            x.append(k)
+    return x
+
+def checkEditTime(times, day, id):
+    #Only show the time of the day that has not been selected before:
+    x = []
+    appointment = Appointment.objects.get(pk=id)
+    time = appointment.time
+    for k in times:
+        if Appointment.objects.filter(day=day, time=k).count() < 1 or time == k:
+            x.append(k)
+    return x
+
+
 #def book(request):
 #    if request.method=="POST":
 #        start_date=request.POST['start_date']
