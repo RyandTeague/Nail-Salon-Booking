db46c22a9f09aa145e8edefbd67b6e7c6aa8320b
RyandTeague <ryanteague@hotmail.co.uk>
2022-11-23 13:01:07 +0000

    migrate booking models

--- Technician/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('',views.dashboard,name="manager_dashboard"),
-    path('new/',views.add_technician,name="add_technician"),
-    path('update/<int:technician_no>/',views.update_technician,name="update_technician"),
-]
--- payment/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .import views
-
-urlpatterns = [
-    # path('', views.initiate_payment, name="initiate-payment"),
-    path('initiate-payment', views.initiate_payment, name="initiate-payment"),
-    path('<str:ref>/', views.verify_payment, name ="verify-payment"),
-]
