4ee56e2d54560848b46a10243e3d9306a36c3055
RyandTeague <ryanteague@hotmail.co.uk>
2022-11-24 12:06:33 +0000

    start writing tests for apps

--- authentication/test_forms.py
+++ authentication/test_forms.py
@@ -1,2 +1,9 @@
 from django.test import TestCase
 from .forms import RegisterUserForm
+
+class TestRegisterUserForm(TestCase):
+    def test_email_is_required(self):
+        form = RegisterUserForm(email = '')
+        self.assertFalse(form.is_valid())
+        self.assertIn('email', form.errors.keys())
+        self.assertEqual(form.errors['name'][0], 'This field is required.')
--- authentication/test_views.py
+++ authentication/test_views.py
@@ -1,3 +1,3 @@
 from django.test import TestCase
-
+from .views import login_user, logout_user, register_user
 # Create your tests here.
